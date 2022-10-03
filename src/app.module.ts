65b398a3c9919da4bcbe092c7627d3db57a0d512
FWiman <frewim@gmail.com>
2022-10-03 11:52:43 +0200

    added docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SwapiModule } from './swapi/swapi.module';
 
 @Module({
-  imports: [SwapiModule,],
+  imports: [SwapiModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 4000,
+      username: 'newtons',
+      password: 'newton123',
+      database: 'Starwars',
+      entities: [],
+      synchronize: false      // WARNING!!!! Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+    })],
 })
 export class AppModule {}
