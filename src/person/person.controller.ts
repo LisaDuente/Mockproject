f5db6a20d46b8d1bdf3d49d09b9687bf8a86a61d
Lisa Dünte <lisa.duente@gmail.com>
2022-10-05 15:13:39 +0200

    added get Requests in Person

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Post } from '@nestjs/common';
 import { PersistedEntityNotFoundError } from 'typeorm';
-import { Person } from '../entity/Person';
-import { PersonDto } from './dto/addPerson.dto';
+import { GetPersonDto } from './dto/getPerson.dto';
 import { PersonService } from './person.service';
 
 @Controller('person')
@@ -11,6 +10,71 @@ export class PersonController {
   @Get('getAll')
   getAll() {
     return this.service.getAll();
+  }
+
+  @Get('getById')
+  getById(@Body() dto: GetPersonDto) {
+    return this.service.getById(dto.id);
+  }
+
+  //Or try the param version
+  /*
+  @Get('getById/:id')
+  getById(@Param("id", ParseIntPipe) id: number) {
+    return this.service.getById(id);
+  }
+  */
+
+  @Get('getByName')
+  getByName(@Body() dto: GetPersonDto){
+    return this.service.getByName(dto.name)
+  }
+
+  @Get('getByBirthday')
+  getByBirthday(@Body() dto: GetPersonDto){
+    return this.service.getByBirthDate(dto.birth_year)
+  }
+
+  @Get("getByEyeColor")
+  getByEyeColor(@Body() dto: GetPersonDto){
+    return this.service.getByEyeColor(dto.eye_color)
+  }
+
+  @Get("getByMass")
+  getByMass(@Body() dto: GetPersonDto){
+    return this.service.getByMass(dto.mass)
+  }
+
+  @Get("getByHeight")
+  getByHeight(@Body() dto: GetPersonDto){
+    return this.service.getByHeight(dto.height)
+  }
+
+  @Get("getByGender")
+  getByGender(@Body() dto: GetPersonDto){
+    return this.service.getByGender(dto.gender)
+  }
+
+  @Get("getByHairColor")
+  getByHair(@Body() dto: GetPersonDto){
+    return this.service.getByHair(dto.hair_color)
+  }
+
+  @Get("getBySkinColor")
+  getBySkin(@Body() dto: GetPersonDto){
+    return this.service.getBySkin(dto.skin_color)
+  }
+
+  @Get("mixParams")
+  getByServeralParams(@Body() dto: GetPersonDto){
+    return this.service.getByServeralParams(dto);
+  }
+
+  //problem: if we want to find a homeworld we need to look up the link in the relation! 
+  //add in SEED: fetch all the data from the other relations to make an array with words?
+  @Get("getByHomeworld")
+  getByHomeWorld(@Body() dto: GetPersonDto){
+    return this.service.getByHomeworld(dto.homeworld);
   }
 
   @Post('test')
--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,15 +1,92 @@
 import { Injectable } from '@nestjs/common';
+import { toNamespacedPath } from 'path';
 import { DataSource } from 'typeorm';
 import { Person } from '../entity/Person';
 import { SwapiService } from '../swapi/swapi.service';
 import { PersonDto } from './dto/addPerson.dto';
+import { GetPersonDto } from './dto/getPerson.dto';
 
 @Injectable()
 export class PersonService {
     constructor(private swapiService: SwapiService){}
+    
+  //added a sort inside of this
   getAll() {
-    return Person.find(); //SELECT * from Person
+    return Person.find({order: {
+      id: "DESC"
+    }}); //SELECT * from Person
   }
+
+  getById(id: number){
+    return Person.findBy({id: id});
+  }
+
+  getByName(name: string){
+    return Person.findBy({name: name});
+  }
+
+  getByBirthDate(birthdate: string){
+    return Person.findByBirthday(birthdate);
+  }
+
+  getByHomeworld(homeworld: string){
+    return Person.findByHomeworld(homeworld);
+  }
+
+  getByEyeColor(color: string){
+    return Person.findByEyeColor(color)
+  }
+
+  getByMass(mass: string){
+    return Person.findByMass(mass)
+  }
+
+  getByHeight(height: string){
+    return Person.findByHeight(height)
+  }
+
+  getByGender(gender: string){
+    return Person.findByGender(gender)
+  }
+
+  getByHair(hair: string){
+    return Person.findByHair(hair)
+  }
+
+  getBySkin(skin: string){
+    return Person.findBySkin(skin)
+  }
+
+  //this returns all people with blue eyes, all people with gender female.... but not all blue eyed females!
+  async getByServeralParams(dto: GetPersonDto){
+    let personList = [];
+    if("name" in dto){
+      for(let person of await Person.findBy({name: dto.name})){
+        personList.push(person)
+      }
+    }
+
+    if("birth_year" in dto){
+      for(let person of await Person.findByBirthday(dto.birth_year)){
+        personList.push(person)
+      }
+    }
+
+    if("eye_color" in dto){
+      for(let person of await Person.findByEyeColor(dto.eye_color)){
+        personList.push(person)
+      }
+    }
+
+    if("gender" in dto){
+      for(let person of await Person.findByGender(dto.gender)){
+        personList.push(person)
+      }
+    }
+
+    return personList
+  }
+
 
   addTest(person: Person) {
     return Person.save({...person});
