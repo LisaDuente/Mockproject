f8a9b3e8b503df7e8a29f02274f23f72e6d07c06
FWiman <frewim@gmail.com>
2022-10-06 13:50:16 +0200

    added delete functions

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Person } from '../entity/Person';
 import { SwapiService } from '../swapi/swapi.service';
 import { PersonDto } from './dto/addPerson.dto';
 
 @Injectable()
 export class PersonService {
-    constructor(private swapiService: SwapiService){}
+    constructor(
+      private swapiService: SwapiService,
+      // @InjectRepository(Person)
+      // private personRepository: Repository<Person>
+      ){}
+      
   getAll() {
     return Person.find(); //SELECT * from Person
   }
@@ -23,5 +29,56 @@ export class PersonService {
     }
     return "seeded"
   }
+
+  // Deletes a row chosen by unique id
+  async deleteOne(id: number) {
+     await Person
+      .createQueryBuilder()
+      .delete()
+      .from(Person)
+      .where('id = :id', {id: id})
+      .execute()
+  }
+
+  async deleteAll() {
+    const dbToDelete = await Person.find()
+    for(let i = 0; i < dbToDelete.length; i++) {
+    await Person
+        .createQueryBuilder()
+        .delete()
+        .from(Person)
+        .where('id = :id', {id: dbToDelete[i].id})
+        .execute()
+
+    }
+
+    return 'Database deleted'
+  }
+
+
+  
+  // "Flags" a row to make it unusable
+  // async softDelete (id: number) {
+  //   const deleteResponse = await this.personRepository.softDelete({id: id});
+  //     if(!deleteResponse.affected) {
+  //       throw new console.error(id);
+  //     }
+  //   //  await Person
+  //   //   .getRepository()
+  //   //   .createQueryBuilder()
+  //   //   .softDelete()
+  //   //   .where('id = :id', {id: id})
+  //   //   .execute()
+  // }
+
+  // // Restores a "flaged" row back to usable
+  // async restoreSoftDelete (id: number) {
+  //   await Person
+  //     .getRepository()
+  //     .createQueryBuilder()
+  //     .restore()
+  //     .where('id = :id', {id: id})
+  //     .execute()
+  // }
   
 }
